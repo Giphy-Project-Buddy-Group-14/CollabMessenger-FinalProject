c67ba95068b70036d6659456bee517e66a130cdb
Nikolay Hadzhiyski <83013870+dkgrayh@users.noreply.github.com>
2023-11-18 06:42:21 -0500

    Added signin and signup methods (#3)

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,12 +5,32 @@ import {
 } from 'firebase/auth';
 import { auth } from '../../firebaseAppConfig';
 
-export const registerUser = (email, password) => {
-  return createUserWithEmailAndPassword(auth, email, password);
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log('User signed up:', user);
+  } catch (error) {
+    console.error('Sign up error:', error.message);
+  }
 };
 
-export const loginUser = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password);
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log('User signIn:', user);
+  } catch (error) {
+    console.error('Sign in error:', error.message);
+  }
 };
 
 export const logoutUser = () => {
