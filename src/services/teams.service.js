62b59f0ce010225e22147f324093805c9f270bad
Nikolay Hadzhiyski <83013870+dkgrayh@users.noreply.github.com>
2023-11-29 15:40:43 -0500

    Updated from username to userUid (#49)

--- src/services/teams.service.js
+++ src/services/teams.service.js
@@ -1,17 +1,17 @@
 import { get, set, ref, update, push } from 'firebase/database';
 import { db } from '../../firebaseAppConfig';
 
-export const addTeam = async (username, name) => {
+export const addTeam = async (userUid, name) => {
   try {
     const result = await push(ref(db, 'teams'), {});
     const uid = result.key;
-    const owner = username;
+    const owner = userUid;
     const channels = {};
     const members = {};
-    members[username] = true;
+    members[userUid] = true;
 
     await set(ref(db, `teams/${uid}`), { name, owner, members, channels, uid });
-    await update(ref(db), { [`users/${username}/MyTeams/${name}`]: uid });
+    await update(ref(db), { [`users/${userUid}/MyTeams/${name}`]: uid });
     return uid;
   } catch (error) {
     console.error('Error adding team:', error);
