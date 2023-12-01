b84b589152b6c6da067a3507224caa16ee46111c
Svilena Slavev <svilslavev@gmail.com>
2023-12-01 13:29:09 +0200

    fix: close the div at the right place to keep the required layout

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -151,47 +151,47 @@ export default function Chat() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <div className="flex flex-col flex-auto h-full p-6">
-            <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
-              {/* ... Chat Messages ... */}
-              {!!selectedChannel && (
-                <div>
-                  <h1 className="text-xl font-semibold mb-6">
-                    {selectedChannel.title}
-                  </h1>
-                  <ul>
-                    {Object.keys(selectedChannel.messages || {}).map(
-                      (messageKey) => {
-                        const message = selectedChannel.messages[messageKey];
-                        return (
-                          <li key={messageKey} className="mb-4">
-                            <div className="flex flex-row items-center justify-between">
-                              <div>
-                                <p className="text-s font-semibold">
-                                  {message.text}
-                                </p>
-                                <div className="text-xs">{message.owner}</div>
-                              </div>
-                              <span className="text-xs text-gray-500">
-                                {message.createdOn}
-                              </span>
-                            </div>
-                          </li>
-                        );
-                      }
-                    )}
-                  </ul>
-                </div>
-              )}
-
-              <div className="flex flex-col justify-end mt-auto">
-                <ChatSection selectedChannel={selectedChannel} />
-              </div>
+      <div className="flex flex-col flex-auto h-full p-6">
+        <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
+          {/* ... Chat Messages ... */}
+          {!!selectedChannel && (
+            <div>
+              <h1 className="text-xl font-semibold mb-6">
+                {selectedChannel.title}
+              </h1>
+              <ul>
+                {Object.keys(selectedChannel.messages || {}).map(
+                  (messageKey) => {
+                    const message = selectedChannel.messages[messageKey];
+                    return (
+                      <li key={messageKey} className="mb-4">
+                        <div className="flex flex-row items-center justify-between">
+                          <div>
+                            <p className="text-s font-semibold">
+                              {message.text}
+                            </p>
+                            <div className="text-xs">{message.owner}</div>
+                          </div>
+                          <span className="text-xs text-gray-500">
+                            {message.createdOn}
+                          </span>
+                        </div>
+                      </li>
+                    );
+                  }
+                )}
+              </ul>
             </div>
+          )}
+
+          <div className="flex flex-col justify-end mt-auto">
+            <ChatSection selectedChannel={selectedChannel} />
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
