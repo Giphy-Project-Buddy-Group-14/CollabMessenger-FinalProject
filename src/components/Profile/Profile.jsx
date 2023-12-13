71d9b9df869205bb6d14e8febf3e03422ec617e5
Nikolay Hadzhiyski <nikolay.hadzhiyski@gmail.com>
2023-12-13 08:35:33 -0500

    Removed edit button for not authenticated profiles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,11 +2,12 @@ import { Link, useParams } from 'react-router-dom'; // Import Link from react-ro
 import ImageWithLoading from '../helper/ImageWithLoading';
 import LoadingIndicator from '../Ui/LoadingIndicator';
 import useLoadUserProfileByUsername from '../../hooks/useLoadUserProfileByUsername';
-
+import { useAuth } from '../../hooks/useAuth';
 export default function Profile() {
   const params = useParams();
   const username = params.username;
   const { loading, userProfile } = useLoadUserProfileByUsername(username);
+  const { currentUserProfile } = useAuth();
 
   return (
     <>
@@ -38,14 +39,16 @@ export default function Profile() {
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Phone: {userProfile.phone}
             </span>
-            <div className="flex mt-4 md:mt-6">
-              <Link
-                to="edit"
-                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Edit
-              </Link>
-            </div>
+            {username === currentUserProfile.username && (
+              <div className="flex mt-4 md:mt-6">
+                <Link
+                  to="edit"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Edit
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       )}
