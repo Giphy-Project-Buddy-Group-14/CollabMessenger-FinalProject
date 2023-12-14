443e0a6a242981000ecb28074a6fb4ff1150747f
Svilena Slavev <svilslavev@gmail.com>
2023-12-14 23:30:37 +0200

    fix: handle missing teams

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -57,28 +57,32 @@ export default function Profile() {
             <div className="flex flex-col gap-2">
               <div className="text-m font-bold">My Teams</div>
 
+              {!currentUserProfile.MyTeams && <div>-</div>}
+
               <ul>
-                {Object.keys(currentUserProfile.MyTeams).map((teamName) => (
-                  <li
-                    className="p-2 px-4 flex gap-2 bg-slate-50 mb-2 rounded-full bg-opacity-60 border pr-8"
-                    key={teamName}
-                  >
-                    <svg
-                      className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 "
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
+                {Object.keys(currentUserProfile?.MyTeams || {}).map(
+                  (teamName) => (
+                    <li
+                      className="p-2 px-4 flex gap-2 bg-slate-50 mb-2 rounded-full bg-opacity-60 border pr-8"
+                      key={teamName}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="1"
-                        d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-                      ></path>
-                    </svg>
-                    <div>{teamName}</div>
-                  </li>
-                ))}
+                      <svg
+                        className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 "
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="1"
+                          d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                        ></path>
+                      </svg>
+                      <div>{teamName}</div>
+                    </li>
+                  )
+                )}
               </ul>
             </div>
           </div>
